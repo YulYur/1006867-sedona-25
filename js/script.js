d3fa39f90b3acbfbef5dd52bf399d2f713448411
YulYur <50015125+YulYur@users.noreply.github.com>
2019-06-21 23:12:04 +0300

    Вторая попытка

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
-    var link = document.querySelector(".hotels-link");
-    var form = document.querySelector(".hotels-form");
+var button = document.querySelector(".hotels-button");
+var form = document.querySelector(".hotels-form");
 
-    var countA = 2;
-    var countC = 0;
-    var countAd = document.getElementById("guest-adults");
-    var countCh = document.getElementById("guest-children");
+var arrivalDate = document.querySelector("[name=arrival-date]");
+var departureDate = document.querySelector("[name=departure-date]");
+
+var countA = 2;
+var countC = 0;
+var countAd = document.getElementById("guest-adults");
+var countCh = document.getElementById("guest-children");
+
+
+button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    form.classList.toggle("hotels-form-close");
+    form.classList.remove("hotels-form-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value) {
+        evt.preventDefault();
+        form.classList.remove("hotels-form-error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add("hotels-form-error");
+    } 
   
-    link.addEventListener("click", function (evt) {
-          evt.preventDefault();
-          form.classList.toggle("hotels-form-close");
-    });
+    if(arrivalDate.value){
+      departureDate.focus();
+    } else {
+      arrivalDate.focus();
+    }
+});
 
-    function plusAd(){
+function plusAd(){
         countA++;
         countAd.value = countA;
     };
     
-    function minusAd(){
+function minusAd(){
       if (countA > 0) {
         countA--;
         countAd.value = countA;
-      }  
-    };
+    }
+};
 
-    function plusCh(){
+function plusCh(){
         countC++;
         countCh.value = countC;
     };
 
-    function minusCh(){
-      if (countC > 0) {
+function minusCh(){
+    if (countC > 0) {
         countC--;
         countCh.value = countC;
-      }  
-    };
+    }
+};
